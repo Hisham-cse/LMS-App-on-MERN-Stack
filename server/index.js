5942baa1ca908d4f92e5205aa441d36ccd3cc406
Muhammad Hisham <hisham.cs.engineer@gmail.com>
2024-12-24 20:50:20 +0530

    rootReducer authapi

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import express from "express";
 import dotenv from "dotenv";
 import ConnectDB from "./database/db.js";
 import userRoute from "./routes/user.route.js"
+import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config({});
 
 // Call databse connection here
@@ -11,15 +13,22 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+//Default MiddleWare
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin:"http://localhost:5174",
+    credentials:true
+}))
 //apis
 app.use("/api/v1/user",userRoute)
-
-app.get("/home",(_,res)=>{
-    res.status(200).json({
-        success:true,
-        message:"Hello i am coming from backend"
-    })
-})
+//"http://localhost:8080/api/v1/user/Register" our api
+// app.get("/home",(_,res)=>{
+//     res.status(200).json({
+//         success:true,
+//         message:"Hello i am coming from backend"
+//     })
+// })
 
 
 app.listen(PORT,()=>{
